456e806fed99aab0630433fd2a12b385f772b6f2
admin <admin@admindeMacBook-Pro.local>
2015-05-14 11:41:46 +0800

    homeItemModel

---
+++ LePats/Model/HomeItemModel.h
@@ -0,0 +1,15 @@
+//
+//  HomeItemModel.h
+//  LePats
+//
+//  Created by admin on 15/5/12.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HomeItemModel : NSObject
+@property (nonatomic,strong) NSString *title;
+@property (nonatomic,strong) NSString *img;
+@property (nonatomic,assign) NSInteger tag;
+@end
---
+++ LePats/Model/HomeItemModel.m
@@ -0,0 +1,13 @@
+//
+//  HomeItemModel.m
+//  LePats
+//
+//  Created by admin on 15/5/12.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import "HomeItemModel.h"
+
+@implementation HomeItemModel
+
+@end
