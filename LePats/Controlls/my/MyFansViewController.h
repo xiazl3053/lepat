0c61d3a040a27c8b0463388642e0b6bc2bc1a461
admin <manuscola@163.com>
2015-06-02 13:02:22 +0800

    06-02 13:02

---
+++ LePats/Controlls/my/MyFansViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyFansViewController.h
+//  LePats
+//
+//  Created by admin on 15/6/2.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MyFansViewController : BaseViewController
+
+@end
